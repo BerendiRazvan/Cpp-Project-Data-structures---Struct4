39affeb818bccd448db0b08ad44688bc66930118
Răzvan Berendi <74509014+BerendiRazvan@users.noreply.github.com>
2021-06-25 20:42:00 +0300

    Update App.cpp

--- App.cpp
+++ App.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include "Matrice.h"
 #include "TestExtins.h"
 #include "TestScurt.h"
@@ -16,11 +16,15 @@ int main() {
 
 
 /*
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 Să se implementeze în C++ un anumit container de date (TAD) folosind o anumită
 reprezentare (indicată) și un arbore binar de căutare (ABC) ca structură de date.
 
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 P1. TAD Matrice - reprezentare sub forma unei matrice rare (triplete de forma <linie, coloană,valoare> (valoare!=0)), 
 memorate folosind un ABC reprezentat înlănțuit cu alocare dinamică.
 
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------
 */
